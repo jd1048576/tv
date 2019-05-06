7a4ce78593ea1e1d335cd82f17ac1e945e4bcf7a
jd1048576 <jd1048576@gmail.com>
2019-05-06 20:29:37 +0100

    Fix manifests

--- app/src/main/java/jdr/tv/app/ui/MainActivity.kt
+++ app/src/main/java/jdr/tv/app/ui/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import jdr.tv.R
+import jdr.tv.app.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
