70d0e025418d5e6d4802b4e038ed5fc77a4aef9a
jd1048576 <jd1048576@gmail.com>
2019-05-18 15:01:03 +0100

    Remove unused logging

--- base-android/src/main/java/jdr/tv/base/android/extensions/Fragment.kt
+++ base-android/src/main/java/jdr/tv/base/android/extensions/Fragment.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import timber.log.Timber
 
 inline val Fragment.appCompatActivity: AppCompatActivity get() = (activity as AppCompatActivity)
 
@@ -14,9 +13,7 @@ inline val Fragment.supportActionBar: ActionBar get() = appCompatActivity.suppor
 fun Fragment.setSupportActionBar(toolbar: Toolbar) = appCompatActivity.setSupportActionBar(toolbar)
 
 fun Fragment.setupToolbar(@IdRes id: Int, @StringRes title: Int = 0, displayHomeAsUp: Boolean = false) {
-    Timber.e(id.toString())
     view?.findViewById<Toolbar>(id)?.also {
-        Timber.e("Title ${it.title}")
         setSupportActionBar(it)
         supportActionBar.apply {
             if (title == 0) this.title = "" else setTitle(title)
