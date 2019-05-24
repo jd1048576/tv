988281e020f47968be5517bb0f323e31895157ad
jd1048576 <jd1048576@gmail.com>
2019-05-24 18:32:04 +0100

    Add menu to top level fragments

--- schedule/src/main/java/jdr/tv/schedule/ui/ScheduleFragment.kt
+++ schedule/src/main/java/jdr/tv/schedule/ui/ScheduleFragment.kt
@@ -1,6 +1,26 @@
 package jdr.tv.schedule.ui
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.fragment.app.Fragment
+import jdr.tv.base.extensions.setupToolbar
 import jdr.tv.schedule.R
 
-class ScheduleFragment : Fragment(R.layout.fragment_schedule)
+class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar(R.id.fragment_schedule_toolbar, R.string.schedule)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+}
--- shows/src/main/java/jdr/tv/shows/ui/ShowsFragment.kt
+++ shows/src/main/java/jdr/tv/shows/ui/ShowsFragment.kt
@@ -1,6 +1,25 @@
 package jdr.tv.shows.ui
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.fragment.app.Fragment
+import jdr.tv.base.extensions.setupToolbar
 import jdr.tv.shows.R
 
-class ShowsFragment : Fragment(R.layout.fragment_shows)
+class ShowsFragment : Fragment(R.layout.fragment_shows) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar(R.id.fragment_shows_toolbar, R.string.shows)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+}
