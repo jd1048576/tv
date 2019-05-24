854dc2a702692bc5e66b87ba0a907cc6faddedca
jd1048576 <jd1048576@gmail.com>
2019-05-24 14:30:10 +0100

    Add Daos and Database

---
+++ local/src/main/java/jdr/tv/local/DetailsDao.kt
@@ -0,0 +1,18 @@
+package jdr.tv.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import jdr.tv.local.entities.DetailedShow
+import jdr.tv.local.entities.Details
+import java.time.Instant
+
+@Dao
+abstract class DetailsDao : BaseDao<Details>() {
+
+    @Query("SELECT * FROM Show JOIN Details ON Show.id = Details.showId WHERE id = :id")
+    abstract fun selectDetailedLiveData(id: Long): LiveData<DetailedShow>
+
+    @Query("SELECT lastDetailsUpdate FROM Details WHERE showId = :id")
+    abstract suspend fun selectLastDetailsUpdate(id: Long): Instant
+}
