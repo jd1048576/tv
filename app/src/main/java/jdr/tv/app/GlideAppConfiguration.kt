27637542f2f738c74b991442d0ed22a9cb4d541e
jd1048576 <jd1048576@gmail.com>
2019-05-28 10:42:01 +0100

    Remove Glide Logging

--- app/src/main/java/jdr/tv/app/GlideAppConfiguration.kt
+++ app/src/main/java/jdr/tv/app/GlideAppConfiguration.kt
@@ -11,7 +11,6 @@ import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-import timber.log.Timber
 import java.io.InputStream
 
 @Excludes(OkHttpLibraryGlideModule::class)
@@ -25,8 +24,6 @@ class GlideAppConfiguration : AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         super.registerComponents(context, glide, registry)
-        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(App.dataComponent(context).client().also {
-            Timber.e("Glide $it")
-        }))
+        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(App.dataComponent(context).client()))
     }
 }
