8aad314cbfa497ede86a1cb0ad3ba09b2bcff98f
jd1048576 <jd1048576@gmail.com>
2019-05-28 11:40:47 +0100

    Fix ViewModel Check

--- search/src/main/java/jdr/tv/search/ui/SearchViewModelProviderFactory.kt
+++ search/src/main/java/jdr/tv/search/ui/SearchViewModelProviderFactory.kt
@@ -7,8 +7,8 @@ import javax.inject.Inject
 class SearchViewModelProviderFactory @Inject constructor(private val repository: SearchRepository) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
-            throw IllegalArgumentException("Unknown ViewModel class $modelClass")
+        if (!modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+            throw IllegalArgumentException("Unknown ViewModel $modelClass")
         }
         return modelClass.cast(SearchViewModel(repository))!!
     }
