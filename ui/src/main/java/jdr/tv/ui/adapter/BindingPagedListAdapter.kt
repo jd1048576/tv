78b8655daa9a61102241ad0bf7ee8c77bdbc26c0
jd1048576 <jd1048576@gmail.com>
2019-06-16 21:20:01 +0100

    Improve BindingPagedListAdapter

--- ui/src/main/java/jdr/tv/ui/adapter/BindingPagedListAdapter.kt
+++ ui/src/main/java/jdr/tv/ui/adapter/BindingPagedListAdapter.kt
@@ -2,35 +2,26 @@ package jdr.tv.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BindingPagedListAdapter<T>(config: AsyncDifferConfig<T>) : PagedListAdapter<T, BindingViewHolder<T>>(config) {
+abstract class BindingPagedListAdapter<T, Binding : ViewDataBinding>(config: AsyncDifferConfig<T>) :
+    PagedListAdapter<T, BindingViewHolder<Binding>>(config) {
 
-    @LayoutRes
-    abstract fun layoutIdForViewType(viewType: Int): Int
+    abstract fun bindingForViewType(viewType: Int, inflater: LayoutInflater, parent: ViewGroup): Binding
 
-    abstract fun variableIdForViewType(viewType: Int): Int
+    abstract fun bind(binding: Binding, item: T?)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<T> {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val viewDataBinding: ViewDataBinding = DataBindingUtil.inflate(layoutInflater, layoutIdForViewType(viewType), parent, false)
-        return BindingViewHolder(variableIdForViewType(viewType), viewDataBinding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<Binding> {
+        return BindingViewHolder(bindingForViewType(viewType, LayoutInflater.from(parent.context), parent))
     }
 
-    override fun onBindViewHolder(holder: BindingViewHolder<T>, position: Int) {
-        holder.bind(getItem(position))
-    }
-}
-
-class BindingViewHolder<T>(private val variableId: Int, private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-
-    fun bind(item: T?) {
-        binding.setVariable(variableId, item)
-        binding.executePendingBindings()
+    override fun onBindViewHolder(holder: BindingViewHolder<Binding>, position: Int) {
+        bind(holder.binding, getItem(position))
+        holder.binding.executePendingBindings()
     }
 }
+
+class BindingViewHolder<Binding : ViewDataBinding>(internal val binding: Binding) : RecyclerView.ViewHolder(binding.root)
