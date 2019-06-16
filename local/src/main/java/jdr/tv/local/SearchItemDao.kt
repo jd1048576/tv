8b80acfc1791fe1e0b5d63f182f1b5055c438192
jd1048576 <jd1048576@gmail.com>
2019-06-16 16:59:55 +0100

    Change SearchItem Dao to select page number and handle duplicates

--- local/src/main/java/jdr/tv/local/SearchItemDao.kt
+++ local/src/main/java/jdr/tv/local/SearchItemDao.kt
@@ -12,8 +12,8 @@ abstract class SearchItemDao : BaseDao<SearchItem>() {
     @Query("SELECT Show.* FROM Show JOIN SearchItem ON Show.id = SearchItem.showId ORDER BY SearchItem.id ASC")
     abstract fun selectSearchDataSourceFactory(): DataSource.Factory<Int, Show>
 
-    @Query("SELECT * FROM SearchItem WHERE showId = :showId")
-    abstract suspend fun selectSearchItem(showId: Long): SearchItem?
+    @Query("SELECT page FROM SearchItem WHERE showId = :showId ORDER BY page DESC")
+    abstract suspend fun selectSearchItemPage(showId: Long): Int?
 
     @Query("DELETE FROM SearchItem")
     abstract suspend fun deleteAll()
