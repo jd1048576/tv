7f080c405d91026471e33522da9af83f824579cf
jd1048576 <jd1048576@gmail.com>
2019-06-16 16:58:49 +0100

    Keep track of requests in BoundaryCallback

--- data/src/main/java/jdr/tv/data/BoundaryCallback.kt
+++ data/src/main/java/jdr/tv/data/BoundaryCallback.kt
@@ -3,16 +3,21 @@ package jdr.tv.data
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PagedList
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
-import retrofit2.Response
+import kotlinx.coroutines.withContext
+import java.util.concurrent.ConcurrentHashMap
 
 class BoundaryCallback<LOCAL, REMOTE>(
     private val scope: CoroutineScope,
-    private val request: (Int) -> (suspend () -> Response<REMOTE>)?,
+    private val request: (Int) -> Request<REMOTE>?,
     private val insert: suspend (REMOTE) -> Unit,
     private val currentPageFunction: suspend (LOCAL) -> Int?,
     private val totalPageFunction: (REMOTE) -> Int
 ) : PagedList.BoundaryCallback<LOCAL>() {
+
+    private val inProgress: MutableSet<Int> = ConcurrentHashMap.newKeySet<Int>()
+    private val completed: MutableSet<Int> = ConcurrentHashMap.newKeySet<Int>()
 
     private var totalPages = 0
 
@@ -20,6 +25,8 @@ class BoundaryCallback<LOCAL, REMOTE>(
     val error = MutableLiveData<Exception>()
 
     override fun onZeroItemsLoaded() {
+        inProgress.clear()
+        completed.clear()
         load()
     }
 
@@ -29,13 +36,19 @@ class BoundaryCallback<LOCAL, REMOTE>(
 
     private fun load(itemAtEnd: LOCAL? = null) = scope.launch {
         val page: Int = itemAtEnd?.let { currentPageFunction(it) }?.takeIf { it < totalPages }?.plus(1) ?: 1
-        if (itemAtEnd == null || page != 1) {
+        if (!inProgress.contains(page) && !completed.contains(page)) {
             request(page)?.apply {
-
                 loading.value = Unit
-                val result = enqueue()
-
+                inProgress.add(page)
+                val result = try {
+                    execute()
+                } finally {
+                    withContext(NonCancellable) {
+                        inProgress.remove(page)
+                    }
+                }
                 result.onSuccess {
+                    completed.add(page)
                     totalPages = totalPageFunction(it)
                     insert(it)
                 }.onError { error.value = it }
