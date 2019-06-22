98acecae62dbc1cc6db5281fb26542bcd3ead8e2
jd1048576 <jd1048576@gmail.com>
2019-06-22 20:32:09 +0100

    Add text creator

---
+++ search/src/main/java/jdr/tv/search/ui/SearchTextCreator.kt
@@ -0,0 +1,21 @@
+package jdr.tv.search.ui
+
+import android.text.SpannableStringBuilder
+import android.text.SpannedString
+import jdr.tv.base.extensions.SYSTEM_ZONE_ID
+import jdr.tv.local.entities.Show
+import java.time.Year
+
+object SearchTextCreator {
+
+    @JvmStatic
+    fun details(show: Show): SpannedString = with(SpannableStringBuilder()) {
+        append(Year.from(show.firstAirDate.atZone(SYSTEM_ZONE_ID)).toString())
+        append(" ")
+        if (show.genreList.isNotEmpty()) {
+            append(" - ")
+            append(show.genreList[0])
+        }
+        SpannedString(this)
+    }
+}
