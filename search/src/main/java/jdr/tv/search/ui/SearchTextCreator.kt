40462210ef7745a6dc3b5d3d6813ba0c1524d7d8
jd1048576 <jd1048576@gmail.com>
2019-06-22 22:19:23 +0100

    Make TextCreator Nullable

--- search/src/main/java/jdr/tv/search/ui/SearchTextCreator.kt
+++ search/src/main/java/jdr/tv/search/ui/SearchTextCreator.kt
@@ -9,12 +9,14 @@ import java.time.Year
 object SearchTextCreator {
 
     @JvmStatic
-    fun details(show: Show): SpannedString = with(SpannableStringBuilder()) {
-        append(Year.from(show.firstAirDate.atZone(SYSTEM_ZONE_ID)).toString())
-        append(" ")
-        if (show.genreList.isNotEmpty()) {
-            append(" - ")
-            append(show.genreList[0])
+    fun details(show: Show?): SpannedString = with(SpannableStringBuilder()) {
+        if (show != null) {
+            append(Year.from(show.firstAirDate.atZone(SYSTEM_ZONE_ID)).toString())
+            append(" ")
+            if (show.genreList.isNotEmpty()) {
+                append(" - ")
+                append(show.genreList[0])
+            }
         }
         SpannedString(this)
     }
