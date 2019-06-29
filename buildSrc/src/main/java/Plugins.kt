51b9780694a0a884710c1c4ae7928df2d118b153
jd1048576 <jd1048576@gmail.com>
2019-06-29 10:50:47 +0100

    Update Plugin: Android Gradle. Update Gradle

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -1,5 +1,5 @@
 object Plugins {
-    const val androidGradle = "com.android.tools.build:gradle:3.5.0-beta04"
+    const val androidGradle = "com.android.tools.build:gradle:3.5.0-beta05"
     const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.Kotlin.version}"
     const val androidApplication = "com.android.application"
     const val androidLibrary = "com.android.library"
