6705e03eccf68e55b5232f011f7b3fde0d49461e
jd1048576 <jd1048576@gmail.com>
2019-06-30 14:34:25 +0100

    Remove Object

--- search/src/main/java/jdr/tv/search/ui/SearchTextCreator.kt
+++ search/src/main/java/jdr/tv/search/ui/SearchTextCreator.kt
@@ -1,3 +1,5 @@
+@file:JvmName("SearchTextCreator")
+
 package jdr.tv.search.ui
 
 import android.text.SpannableStringBuilder
@@ -6,17 +8,13 @@ import jdr.tv.base.extensions.SYSTEM_ZONE_ID
 import jdr.tv.local.entities.Show
 import java.time.Year
 
-object SearchTextCreator {
-
-    @JvmStatic
-    fun details(show: Show?): SpannedString = with(SpannableStringBuilder()) {
-        if (show != null) {
-            append(Year.from(show.firstAirDate.atZone(SYSTEM_ZONE_ID)).toString())
-            if (show.genreList.isNotEmpty()) {
-                append(" - ")
-                append(show.genreList[0])
-            }
+fun details(show: Show?): SpannedString = with(SpannableStringBuilder()) {
+    if (show != null) {
+        append(Year.from(show.firstAirDate.atZone(SYSTEM_ZONE_ID)).toString())
+        if (show.genreList.isNotEmpty()) {
+            append(" - ")
+            append(show.genreList[0])
         }
-        SpannedString(this)
     }
+    SpannedString(this)
 }
