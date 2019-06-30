9fd48720699dce7cb36f667dc89995da00ea55a2
jd1048576 <jd1048576@gmail.com>
2019-06-30 22:05:02 +0100

    Add Detailed Show Request Implementation

--- details/src/main/java/jdr/tv/details/ui/DetailsFragment.kt
+++ details/src/main/java/jdr/tv/details/ui/DetailsFragment.kt
@@ -3,12 +3,18 @@ package jdr.tv.details.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import jdr.tv.base.extensions.setupToolbar
+import jdr.tv.data.onFailure
+import jdr.tv.data.onLoading
+import jdr.tv.data.onSuccess
 import jdr.tv.details.R
 import jdr.tv.details.di.inject
+import timber.log.Timber
 import javax.inject.Inject
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
+
 
     @Inject
     lateinit var viewModel: DetailsViewModel
@@ -16,15 +22,15 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         inject()
         setupToolbar(R.id.toolbar, displayHomeAsUp = true)
+        observe()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel.restore(savedInstanceState)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        viewModel.save(outState)
+    private fun observe() {
+        viewModel.id = arguments?.getLong("SHOW_ID")!!
+        viewModel.selectDetailedShow().observe(viewLifecycleOwner, Observer { result ->
+            result.onLoading { Timber.e("LOADING") }
+                .onSuccess { Timber.e("$it") }
+                .onFailure { Timber.e("FAILURE $it") }
+        })
     }
 }
--- details/src/main/java/jdr/tv/details/ui/DetailsViewModel.kt
+++ details/src/main/java/jdr/tv/details/ui/DetailsViewModel.kt
@@ -1,5 +1,21 @@
 package jdr.tv.details.ui
 
-import jdr.tv.viewmodel.StateViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import jdr.tv.data.Result
+import jdr.tv.local.entities.DetailedShow
 
-class DetailsViewModel(private val repository: DetailsRepository) : StateViewModel<DetailsViewState>(DetailsViewState())
+class DetailsViewModel(private val repository: DetailsRepository) : ViewModel() {
+
+    private val _id = MutableLiveData<Long>()
+
+    var id: Long
+        get() = _id.value ?: 0
+        set(value) {
+            _id.value = value
+        }
+
+    fun selectDetailedShow(): LiveData<Result<DetailedShow>> = _id.switchMap { repository.selectDetailedShow(it) }
+}
