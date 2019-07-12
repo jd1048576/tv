ac175b4938527b47745382910f012abe9d2f3216
jd1048576 <jd1048576@gmail.com>
2019-07-12 14:17:25 +0100

    Add Network Call to Fetch Show Details and Seasons

--- details/src/main/java/jdr/tv/details/ui/DetailsFragment.kt
+++ details/src/main/java/jdr/tv/details/ui/DetailsFragment.kt
@@ -3,10 +3,15 @@ package jdr.tv.details.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import jdr.tv.base.extensions.setupToolbar
+import jdr.tv.data.onFailure
+import jdr.tv.data.onLoading
+import jdr.tv.data.onSuccess
 import jdr.tv.details.R
 import jdr.tv.details.di.inject
 import jdr.tv.navigation.GlobalActions
+import timber.log.Timber
 import javax.inject.Inject
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
@@ -22,5 +27,10 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private fun observe() {
         viewModel.id = GlobalActions.ActionDetails.fromBundle(arguments).showId
+        viewModel.selectDetailedShow().observe(viewLifecycleOwner, Observer { result ->
+            result.onLoading { Timber.e("LOADING") }
+                .onSuccess { Timber.e("$it") }
+                .onFailure { Timber.e("FAILURE $it") }
+        })
     }
 }
--- details/src/main/java/jdr/tv/details/ui/DetailsViewModel.kt
+++ details/src/main/java/jdr/tv/details/ui/DetailsViewModel.kt
@@ -1,7 +1,11 @@
 package jdr.tv.details.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import jdr.tv.data.Result
+import jdr.tv.local.entities.DetailedShow
 
 class DetailsViewModel(private val repository: DetailsRepository) : ViewModel() {
 
@@ -12,4 +16,6 @@ class DetailsViewModel(private val repository: DetailsRepository) : ViewModel() 
         set(value) {
             _id.value = value
         }
+
+    fun selectDetailedShow(): LiveData<Result<DetailedShow>> = _id.switchMap { repository.selectDetailedShow(it) }
 }
