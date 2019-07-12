cbea7b192f314daf0b7b3a649a9b68427ba82588
jd1048576 <jd1048576@gmail.com>
2019-07-12 10:20:51 +0100

    Refactor Daos

--- local/src/main/java/jdr/tv/local/Database.kt
+++ local/src/main/java/jdr/tv/local/Database.kt
@@ -5,18 +5,30 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import jdr.tv.local.converters.InstantTypeConverter
 import jdr.tv.local.converters.ListTypeConverter
+import jdr.tv.local.entities.Details
+import jdr.tv.local.entities.Episode
+import jdr.tv.local.entities.RelatedShow
 import jdr.tv.local.entities.SearchItem
+import jdr.tv.local.entities.Season
 import jdr.tv.local.entities.Show
 
 @Database(
-    entities = [Show::class, SearchItem::class],
+    entities = [Details::class, Episode::class, RelatedShow::class, Show::class, SearchItem::class, Season::class],
     exportSchema = false,
     version = 1
 )
 @TypeConverters(InstantTypeConverter::class, ListTypeConverter::class)
 abstract class Database : RoomDatabase() {
 
+    abstract fun detailsDao(): DetailsDao
+
+    abstract fun episodeDao(): EpisodeDao
+
+    abstract fun relatedShowDao(): RelatedShowDao
+
     abstract fun showDao(): ShowDao
 
     abstract fun searchItemDao(): SearchItemDao
+
+    abstract fun seasonDao(): SeasonDao
 }
--- local/src/main/java/jdr/tv/local/EpisodeDao.kt
+++ local/src/main/java/jdr/tv/local/EpisodeDao.kt
@@ -3,46 +3,13 @@ package jdr.tv.local
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
-import androidx.room.Transaction
-import jdr.tv.base.extensions.INSTANT_ZERO
-import jdr.tv.local.entities.Cast
-import jdr.tv.local.entities.Crew
 import jdr.tv.local.entities.Episode
-import java.time.Instant
 
 @Dao
 abstract class EpisodeDao : BaseDao<Episode>() {
 
     @Query("SELECT Episode.* FROM Episode JOIN Season ON Episode.seasonId = Season.id WHERE Season.showId = :showId ORDER BY (Episode.seasonNumber * 1000 + Episode.episodeNumber) ASC")
     abstract fun selectListLiveData(showId: Long): LiveData<List<Episode>>
-
-    @Query("UPDATE Episode SET watched = :watched, watchedAt = :watchedAt WHERE id = :id")
-    abstract suspend fun updateWatched(id: Long, watched: Boolean, watchedAt: Instant)
-
-    @Transaction
-    open suspend fun updateWatched(episodeList: List<Episode>) {
-        val instant = Instant.now()
-        episodeList.forEach { updateWatched(it.id, !it.watched, if (it.watched) INSTANT_ZERO else instant) }
-    }
-
-    @Query("UPDATE Episode SET watched = 0, watchedAt = 0 WHERE seasonId IN (SELECT seasonId FROM Season WHERE showId = :showId)")
-    abstract suspend fun updateWatchedShow(showId: Long)
-
-    @Query("UPDATE Episode SET airDate = :airDate, episodeNumber = :episodeNumber, name = :name, overview = :overview, seasonId = :seasonId, seasonNumber = :seasonNumber, stillPath = :stillPath, voteAverage = :voteAverage, voteCount = :voteCount, crewList = :crewList, guestStarList = :guestStarList WHERE id = :id")
-    abstract suspend fun updateEpisode(
-        airDate: Instant,
-        episodeNumber: Int,
-        id: Long,
-        name: String,
-        overview: String,
-        seasonId: Long,
-        seasonNumber: Int,
-        stillPath: String,
-        voteAverage: Double,
-        voteCount: Int,
-        crewList: List<Crew>,
-        guestStarList: List<Cast>
-    )
 
     /*  @Query("SELECT Episode.*, Show.name AS showName, Show.posterPath AS showPosterPath, Show.launchId, Show.launchSource FROM Episode, Show WHERE Episode.showId = Show.id AND DATE(DATETIME(Episode.airDate / 1000 , 'unixepoch', 'localtime')) >= DATE(DATETIME('now', 'localtime')) GROUP BY Episode.showId HAVING MIN(Episode.airDate + Episode.episodeNumber) ORDER BY Episode.airDate ASC")
     LiveData<List<EpisodeItem>> getScheduleList();
