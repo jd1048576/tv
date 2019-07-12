7e64e553891ae9d35d84e5774842b33e7c593c94
jd1048576 <jd1048576@gmail.com>
2019-07-12 20:57:14 +0100

    Detekt Suppress/UpdateConfig: TooManyFunctions

--- local/src/main/java/jdr/tv/local/converters/ListTypeConverter.kt
+++ local/src/main/java/jdr/tv/local/converters/ListTypeConverter.kt
@@ -12,6 +12,7 @@ import jdr.tv.local.entities.ProductionCompany
 import jdr.tv.local.entities.Video
 import java.io.IOException
 
+@Suppress("TooManyFunctions")
 object ListTypeConverter {
 
     @TypeConverter
