dc56c208f623b933342cd5b16b7b55c755b38126
jd1048576 <jd1048576@gmail.com>
2019-07-12 20:48:49 +0100

    Detekt Fix: LongParameterList, NestedBlockDepth

--- ui/src/main/java/jdr/tv/ui/utils/SpacingItemDecoration.kt
+++ ui/src/main/java/jdr/tv/ui/utils/SpacingItemDecoration.kt
@@ -9,15 +9,13 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 object SpacingItemDecoration {
 
-    private fun setGridItemOffsets(outRect: Rect, position: Int, column: Int, spanCount: Int, horizontalSpacing: Int, verticalSpacing: Int) {
-        outRect.apply {
-            left = (spanCount - column) * horizontalSpacing / spanCount
-            right = horizontalSpacing * (column + 1) / spanCount
-            if (position < spanCount) {
-                top = verticalSpacing
-            }
-            bottom = verticalSpacing
+    private fun Rect.setGridItemOffsets(position: Int, column: Int, spanCount: Int, horizontalSpacing: Int, verticalSpacing: Int) {
+        left = (spanCount - column) * horizontalSpacing / spanCount
+        right = horizontalSpacing * (column + 1) / spanCount
+        if (position < spanCount) {
+            top = verticalSpacing
         }
+        bottom = verticalSpacing
     }
 
     class LinearLayout(private val horizontalSpacing: Int, private val verticalSpacing: Int) : RecyclerView.ItemDecoration() {
@@ -26,26 +24,27 @@ object SpacingItemDecoration {
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
             val layoutManager = parent.layoutManager
             if (layoutManager is LinearLayoutManager) {
-                val position = parent.getChildAdapterPosition(view)
-                outRect.apply {
-                    if (layoutManager.orientation == RecyclerView.VERTICAL) {
-                        if (position == 0) {
-                            top = verticalSpacing
-                        }
-                        bottom = verticalSpacing
-                        left = horizontalSpacing
-                        right = horizontalSpacing
-                    } else {
-                        top = verticalSpacing
-                        bottom = verticalSpacing
-                        if (position == 0) {
-                            left = horizontalSpacing
-                        }
-                        right = horizontalSpacing
-                    }
-                }
+                outRect.setOffsets(layoutManager.orientation, parent.getChildAdapterPosition(view))
             } else {
                 throw IllegalArgumentException("Invalid Layout Manager $layoutManager")
+            }
+        }
+
+        private fun Rect.setOffsets(orientation: Int, position: Int) {
+            if (orientation == RecyclerView.VERTICAL) {
+                if (position == 0) {
+                    top = verticalSpacing
+                }
+                bottom = verticalSpacing
+                left = horizontalSpacing
+                right = horizontalSpacing
+            } else {
+                top = verticalSpacing
+                bottom = verticalSpacing
+                if (position == 0) {
+                    left = horizontalSpacing
+                }
+                right = horizontalSpacing
             }
         }
     }
@@ -58,7 +57,7 @@ object SpacingItemDecoration {
             if (layoutManager is GridLayoutManager) {
                 val position = parent.getChildAdapterPosition(view)
                 val spanCount = layoutManager.spanCount
-                setGridItemOffsets(outRect, position, position % spanCount, spanCount, horizontalSpacing, verticalSpacing)
+                outRect.setGridItemOffsets(position, position % spanCount, spanCount, horizontalSpacing, verticalSpacing)
             } else {
                 throw IllegalArgumentException("Invalid Layout Manager $layoutManager")
             }
@@ -73,7 +72,7 @@ object SpacingItemDecoration {
             if (layoutManager is StaggeredGridLayoutManager) {
                 val position = parent.getChildAdapterPosition(view)
                 val layoutParams = view.layoutParams as StaggeredGridLayoutManager.LayoutParams
-                setGridItemOffsets(outRect, position, layoutParams.spanIndex, layoutManager.spanCount, horizontalSpacing, verticalSpacing)
+                outRect.setGridItemOffsets(position, layoutParams.spanIndex, layoutManager.spanCount, horizontalSpacing, verticalSpacing)
             } else {
                 throw IllegalArgumentException("Invalid Layout Manager $layoutManager")
             }
