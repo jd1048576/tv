3d8a19d773a1aaff39d1b0f8d84ca97e6bc6b50d
jd1048576 <jd1048576@gmail.com>
2019-08-11 12:08:33 +0100

    Make ViewModelProviderFactory return Non-Null ViewModels

--- viewmodel/src/main/java/jdr/tv/viewmodel/ViewModelProviderFactory.kt
+++ viewmodel/src/main/java/jdr/tv/viewmodel/ViewModelProviderFactory.kt
@@ -3,9 +3,9 @@ package jdr.tv.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ViewModelProviderFactory(private val viewModelProvider: () -> ViewModel) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        val viewModel: ViewModel = viewModelProvider()
+class ViewModelProviderFactory(private val provider: () -> ViewModel) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val viewModel: ViewModel = provider()
         if (!modelClass.isAssignableFrom(viewModel::class.java)) {
             throw IllegalArgumentException("Unknown ViewModel $modelClass")
         }
