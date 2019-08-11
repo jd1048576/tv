5c6387c8ee7525b1ea6a804caad9e3ecd3e4409f
jd1048576 <jd1048576@gmail.com>
2019-08-11 15:16:04 +0100

    Disable Glide Manifest Parsing and Change Default install location to auto

--- app/src/main/java/jdr/tv/app/GlideAppConfiguration.kt
+++ app/src/main/java/jdr/tv/app/GlideAppConfiguration.kt
@@ -5,16 +5,13 @@ import android.util.Log
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
-import com.bumptech.glide.annotation.Excludes
 import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import java.io.InputStream
 
-@Excludes(OkHttpLibraryGlideModule::class)
 @GlideModule
 class GlideAppConfiguration : AppGlideModule() {
 
@@ -30,5 +27,9 @@ class GlideAppConfiguration : AppGlideModule() {
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         super.registerComponents(context, glide, registry)
         registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(App.dataComponent(context).client()))
+    }
+
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
     }
 }
