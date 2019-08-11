6f1bec35e5017d6339ff2b086c1aff7c5864ab22
jd1048576 <jd1048576@gmail.com>
2019-08-11 13:43:03 +0100

    Add Detailed Episode and Season

---
+++ local/src/main/java/jdr/tv/local/entities/DetailedSeason.kt
@@ -0,0 +1,13 @@
+package jdr.tv.local.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class DetailedSeason(
+    @Embedded
+    val season: Season,
+    @Relation(parentColumn = "id", entityColumn = "seasonId", entity = Episode::class)
+    val episodeList: List<DetailedEpisode>
+) {
+    val watchCount: Int get() = episodeList.count { it.watched }
+}
