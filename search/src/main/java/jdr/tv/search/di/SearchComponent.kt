6a81a3daeb9c39ede84fef55a2a0d9e94de9c5bf
jd1048576 <jd1048576@gmail.com>
2019-09-21 12:43:19 +0100

    Change BaseComponent to Injector

--- search/src/main/java/jdr/tv/search/di/SearchComponent.kt
+++ search/src/main/java/jdr/tv/search/di/SearchComponent.kt
@@ -2,14 +2,14 @@ package jdr.tv.search.di
 
 import dagger.BindsInstance
 import dagger.Component
-import jdr.tv.base.di.BaseComponent
 import jdr.tv.base.di.FragmentScope
+import jdr.tv.base.di.Injector
 import jdr.tv.data.di.DataComponent
 import jdr.tv.search.ui.SearchFragment
 
 @FragmentScope
 @Component(modules = [SearchModule::class], dependencies = [DataComponent::class])
-interface SearchComponent : BaseComponent<SearchFragment> {
+interface SearchComponent : Injector<SearchFragment> {
 
     @Component.Factory
     interface Factory {
