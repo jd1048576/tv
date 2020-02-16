a0f97a5c5b271ea1c5dc7e6f1e6304c3f4e54cb6
jd1048576 <jd1048576@gmail.com>
2020-02-16 16:59:55 +0000

    Add WatchList Query

--- data/local/src/main/java/jdr/tv/data/local/EpisodeDao.kt
+++ data/local/src/main/java/jdr/tv/data/local/EpisodeDao.kt
@@ -2,11 +2,22 @@ package jdr.tv.data.local
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import jdr.tv.data.local.entities.Episode
+import jdr.tv.data.local.entities.EpisodeItem
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EpisodeDao : BaseDao<Episode> {
+
+    @Transaction
+    @Query("SELECT Episode.episodeNumber, Episode.name, Episode.id, Episode.seasonNumber, " +
+        "Show.id as showId, Show.name as showName, Show.posterPath FROM Episode " +
+        "JOIN Season ON Season.id = Episode.seasonId JOIN Show ON Show.id = Season.showId " +
+        "WHERE Episode.airDate < strftime('%s','now') AND Show.id IN (SELECT `Add`.showId FROM `Add`) " +
+        "AND Episode.id NOT IN (SELECT episodeId FROM Watch) " +
+        "GROUP BY Show.id HAVING MIN(Episode.seasonNumber * 1000 + Episode.episodeNumber)")
+    fun selectWatchList(): Flow<List<EpisodeItem>>
 
     @Query(
         "SELECT Episode.* FROM Episode " +
