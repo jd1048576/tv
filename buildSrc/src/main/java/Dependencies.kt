a4b60be53a8b5fb99932b6bcb9dd785b6f62aef6
jd1048576 <jd1048576@gmail.com>
2020-02-21 16:25:08 +0000

    Update Dependencies and Gradle

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -23,13 +23,13 @@ const val KOTLIN_COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-cor
 const val KOTLIN_COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$KOTLIN_COROUTINES_VERSION"
 
 private const val ANDROIDX_LIFECYCLE_VERSION = "2.2.0"
-private const val ANDROIDX_NAVIGATION_VERSION = "2.3.0-alpha01"
-private const val ANDROIDX_ROOM_VERSION = "2.2.3"
+private const val ANDROIDX_NAVIGATION_VERSION = "2.3.0-alpha02"
+private const val ANDROIDX_ROOM_VERSION = "2.2.4"
 const val ANDROIDX_ACTIVITY = "androidx.activity:activity:1.1.0"
 const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:1.2.0-alpha02"
 const val ANDROIDX_CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
 const val ANDROIDX_CORE = "androidx.core:core-ktx:1.3.0-alpha01"
-const val ANDROIDX_FRAGMENT = "androidx.fragment:fragment-ktx:1.2.1"
+const val ANDROIDX_FRAGMENT = "androidx.fragment:fragment-ktx:1.2.2"
 const val ANDROIDX_LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:$ANDROIDX_LIFECYCLE_VERSION"
 const val ANDROIDX_LIFECYCLE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:$ANDROIDX_LIFECYCLE_VERSION"
 const val ANDROIDX_LIFECYCLE_VIEWMODEL_SAVEDSTATE = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-rc01"
@@ -43,7 +43,7 @@ const val ANDROIDX_ROOM = "androidx.room:room-ktx:$ANDROIDX_ROOM_VERSION"
 const val ANDROIDX_ROOM_COMMON = "androidx.room:room-common:$ANDROIDX_ROOM_VERSION"
 const val ANDROIDX_ROOM_COMPILER = "androidx.room:room-compiler:$ANDROIDX_ROOM_VERSION"
 const val ANDROIDX_VIEWPAGER2 = "androidx.viewpager2:viewpager2:1.0.0"
-const val ANDROIDX_WORK = "androidx.work:work-runtime-ktx:2.3.1"
+const val ANDROIDX_WORK = "androidx.work:work-runtime-ktx:2.3.2"
 
 const val COIL = "io.coil-kt:coil:0.9.5"
 
@@ -56,13 +56,13 @@ const val DAGGER_COMPILER = "com.google.dagger:dagger-compiler:$DAGGER_VERSION"
 const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:17.2.2"
 const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics:17.0.0-beta01"
 
-const val MATERIAL = "com.google.android.material:material:1.2.0-alpha04"
+const val MATERIAL = "com.google.android.material:material:1.2.0-alpha05"
 
 private const val MOSHI_VERSION = "1.9.2"
 const val MOSHI = "com.squareup.moshi:moshi:$MOSHI_VERSION"
 const val MOSHI_CODEGEN = "com.squareup.moshi:moshi-kotlin-codegen:$MOSHI_VERSION"
 
-const val OKHTTP3 = "com.squareup.okhttp3:okhttp:4.3.1"
+const val OKHTTP3 = "com.squareup.okhttp3:okhttp:4.4.0"
 
 private const val RETROFIT_VERSION = "2.7.1"
 const val RETROFIT = "com.squareup.retrofit2:retrofit:$RETROFIT_VERSION"
