d2b402c3848871ef8f70c702b98a54381c66e245
jd1048576 <jd1048576@gmail.com>
2020-03-29 16:08:11 +0100

    Move Work Initialisation

--- data/core/src/main/java/jdr/tv/data/core/di/DataComponentApplication.kt
+++ data/core/src/main/java/jdr/tv/data/core/di/DataComponentApplication.kt
@@ -2,29 +2,21 @@ package jdr.tv.data.core.di
 
 import android.app.Application
 import android.content.Context
-import androidx.work.Configuration
 import javax.inject.Inject
 import jdr.tv.data.core.initialization.AppInitializer
 
-open class DataComponentApplication : Application(), Configuration.Provider {
+open class DataComponentApplication : Application() {
 
     private lateinit var dataComponent: DataComponent
 
     @Inject
     lateinit var initializer: AppInitializer
 
-    @Inject
-    lateinit var workConfiguration: Configuration
-
     override fun onCreate() {
         super.onCreate()
         dataComponent = DaggerDataComponent.factory().create(this)
         dataComponent.inject(this)
         initializer.initialize()
-    }
-
-    override fun getWorkManagerConfiguration(): Configuration {
-        return workConfiguration
     }
 
     companion object {
--- data/core/src/main/java/jdr/tv/data/core/initialization/WorkInitializer.kt
+++ data/core/src/main/java/jdr/tv/data/core/initialization/WorkInitializer.kt
@@ -1,13 +1,15 @@
 package jdr.tv.data.core.initialization
 
 import android.content.Context
+import androidx.work.Configuration
 import androidx.work.WorkManager
 import jdr.tv.data.work.SyncWorker
 import jdr.tv.data.work.extensions.enqueueUniquePeriodicWork
 import javax.inject.Inject
 
-class WorkInitializer @Inject constructor(private val context: Context) : Initializer {
+class WorkInitializer @Inject constructor(private val context: Context, private val configuration: Configuration) : Initializer {
     override fun initialize() {
+        WorkManager.initialize(context, configuration)
         WorkManager.getInstance(context).apply {
             enqueueUniquePeriodicWork<SyncWorker>(SyncWorker.createPeriodicWorkRequest())
         }
